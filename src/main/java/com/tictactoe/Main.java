bbdf7aa0047e979b88a189af95abce5075d414a0
Michal Kryszewski <m.kryszewski@gmail.com>
2025-01-05 12:08:44 +0100

    michal-kryszewski-tictactoe First Commit

--- src/main/java/com/tictactoe/Main.java
+++ src/main/java/com/tictactoe/Main.java
@@ -1,4 +1,4 @@
-package com.kodilla;
+package com.tictactoe;
 
 public class Main {
     public static void main(String[] args) {
---
+++ tictactoe-game/src/main/java/com/tictactoe/game/Board.java
@@ -0,0 +1,20 @@
+package com.tictactoe.game;
+
+import java.util.List;
+
+public class Board {
+
+    List<String> fields;
+
+    public void insertCircle() {
+
+    }
+
+    public void insertCross() {
+
+    }
+
+    public String checkWinner() {
+        return null;
+    }
+}
